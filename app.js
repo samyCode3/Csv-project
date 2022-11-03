4ad0a3041c4aecf0351a9816af0fe3405ac1c190
samson onifade <107084481+samyCode3@users.noreply.github.com>
2022-11-03 13:11:15 +0100

    update

--- app.js
+++ app.js
@@ -1,7 +1,60 @@
-const csv_converter = require("./utills/input")
-const  myFilePath = "./Files/HNGi9 CSV FILE - Sheet1.csv"
-
-csv_converter.Links(myFilePath)
-
+const csv = require("./csv/csv_json")
+node_cj = require("node-csv-json")
+const crypto = require("crypto")
 
 
+const fs = require("fs")
+const json = fs.readFileSync("output.json","utf-8")
+  function sha256(content) {  
+    return crypto.createHash('sha256').update(content).digest('hex')
+  }
+
+ const ashCreates =  sha256(json)
+
+ let arr = JSON.parse(json);
+ 
+arr.forEach(nft => {
+  nft.mechanic_boot = `${ashCreates}`;
+});
+
+
+
+let arrayS = JSON.stringify(arr)
+
+
+fs.writeFileSync("./updatedjson",`${arrayS}`)
+
+const { Parser } = require('json2csv');
+
+let myData = fs.readFileSync("updatedjson","utf-8")
+// console.log(myData)
+
+myData = JSON.parse(myData);
+ 
+const opts = { myData };
+
+try {
+  const parser = new Parser();
+  const csv = parser.parse(myData,opts);
+  console.log(csv)
+} catch (err) {
+  console.error(err);
+}
+
+fs.writeFileSync("./finalcsv",`${csv}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+  
---
+++ csv/csv_json.js
@@ -0,0 +1,6 @@
+const fs = require("fs")
+
+const csv = fs.readFileSync("./csv/HNGi9 CSV FILE - Sheet1.csv","utf-8")
+
+
+module.exports = csv
