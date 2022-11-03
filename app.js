556cd7a9aca8195bed94bd3f925666a82faca13a
samson onifade <107084481+samyCode3@users.noreply.github.com>
2022-11-03 14:35:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ const crypto = require("crypto")
 
 
 const fs = require("fs")
+node_cj({
+  input: csv,
+  output: "output.json"
+}, function(err, result){
+  if(err) {
+    console.error(err);
+  }else {
+    console.log("done")
+  }
+});
+
 const json = fs.readFileSync("output.json","utf-8")
   function sha256(content) {  
     return crypto.createHash('sha256').update(content).digest('hex')
   }
+
 
  const ashCreates =  sha256(json)
 
